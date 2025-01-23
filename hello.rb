8525254affa5f0a519bb93d945bbf81998c879d4
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-01-23 15:26:01 +0000

    Class intro to gemfile

---
+++ hello.rb
@@ -0,0 +1,14 @@
+require "active_support/all"
+require "awesome_print"
+require "pry-byebug"
+
+# extract lat and long
+# call API that returns current weather
+
+pp "Where are you located?"
+
+x = gets.chomp
+
+pp x
+
+pp "Test"
